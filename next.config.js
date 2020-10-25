fe3f0cc5d82d74ff19e74b896d81cd503ba74b6a
Prashanth <prasanth@primaseller.com>
2020-10-25 13:49:17 +0530

    i18n update in next.config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,31 @@
+const path = require('path');
+const { nextI18NextRewrites } = require('next-i18next/rewrites');
+const localeSubpaths = {
+  as: 'assamese',
+  be: 'bengali',
+  gu: 'gujarati',
+  hi: 'hindi',
+  ka: 'kannada',
+  ma: 'malayalam',
+  mr: 'marathi',
+  or: 'oriya',
+  pan: 'punjabi',
+  ur: 'urdu',
+  en: 'english',
+  te: 'telugu',
+  ta: 'tamil',
+};
 module.exports = {
   distDir: 'dist',
+  rewrites: async () => nextI18NextRewrites(localeSubpaths),
+  publicRuntimeConfig: {
+    localeSubpaths,
+  },
+
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // if (!isServer) {
     //   webpack.resolve = { alias: require("./aliases.config").webpack };
     // }
     return config;
-  }
+  },
 };
