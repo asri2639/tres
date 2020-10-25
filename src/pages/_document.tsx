16fca5892c86467013f276fcbd63c14b17cab4e5
Prashanth <prasanth@primaseller.com>
2020-10-25 13:48:49 +0530

    language change detection in app.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,11 +3,13 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 export default class ETVDocument extends Document {
     render() {
         return (
-            <Html>
-                <Head></Head>
-                
+            <Html lang='en'>
+                <Head>
+                </Head>
                 <body>
                     <Main />
+                    {/* Here we will mount our modal portal */}
+                    <div id="modal" style={{ height: 'auto' }} />
                 </body>
                 <NextScript />
             </Html>
