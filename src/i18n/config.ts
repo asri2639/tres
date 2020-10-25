39ca95ec5eeac858f40aebff1e129e70cbe89f09
Prashanth <prasanth@primaseller.com>
2020-10-25 13:56:31 +0530

    article slug and article component done

---
+++ src/i18n/config.ts
@@ -0,0 +1,9 @@
+export const defaultLocale = 'english' as const;
+
+export const locales = ['english', 'telugu', 'tamil'] as const;
+
+export const languageNames = {
+  english: 'english',
+  telugu: 'telugu',
+  tamil: 'tamil',
+};
