7ba7dfd49312e647df5aa2fdda272fe9f6100321
Prashanth <prasanth@primaseller.com>
2020-10-25 13:56:16 +0530

    i18n related files addition

---
+++ src/i18n/strings.ts
@@ -0,0 +1,25 @@
+import { Strings } from '@i18n/types';
+
+const strings: Strings = {
+  english: {
+    about: 'About René Magritte',
+    painting: 'The Painting',
+    top_news: 'Top News',
+    // (...)
+  },
+  telugu: {
+    about: 'Sur René Magritte',
+    painting: 'La peinture',
+    top_news: 'Top News1',
+
+    // (...)
+  },
+  tamil: {
+    about: 'O René Magritte',
+    painting: 'Obraz',
+    top_news: 'Top News2',
+
+    // (...)
+  },
+};
+export default strings;
