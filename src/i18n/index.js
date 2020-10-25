3da52f18169f17700f056f813d1fb7603c73de10
Prashanth <prasanth@primaseller.com>
2020-10-25 13:56:16 +0530

    i18n related files addition

---
+++ src/i18n/index.js
@@ -0,0 +1,26 @@
+const NextI18Next = require('next-i18next').default;
+const { localeSubpaths } = require('next/config').default().publicRuntimeConfig;
+const path = require('path');
+
+module.exports = new NextI18Next({
+  defaultLanguage: 'en',
+  defaultNS: 'common',
+  otherLanguages: [
+    'as',
+    'be',
+    'gu',
+    'hi',
+    'ka',
+    'ma',
+    'mr',
+    'or',
+    'pan',
+    'ur',
+    'en',
+    'te',
+    'ta',
+    'pan',
+  ],
+  localeSubpaths,
+  localePath: path.resolve('./public/assets/locales'),
+});
