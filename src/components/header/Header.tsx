b9c2f3b424728e347759ddcf905846d6c0424139
Prashanth <prasanth@primaseller.com>
2020-10-27 10:36:25 +0530

    header module updated with initial category on hover

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,26 @@
 import DesktopHeader from "@components/header/DesktopHeader";
 import MobileHeader from "@components/header/MobileHeader";
+import { Media, MediaContextProvider } from "media";
 
 export default function Header({ data }) {
     return (
         <>
-            <DesktopHeader data={data} />
-            <MobileHeader data={data} />
+            <MediaContextProvider>
+                <Media at="xs">
+                    {(mediaClassNames, renderChildren) => {
+                        return (
+                            <MobileHeader className={mediaClassNames} data={data} />
+                        )
+                    }}
+                </Media>
+                <Media greaterThan="xs">
+                    {(mediaClassNames, renderChildren) => {
+                        return (
+                            <DesktopHeader className={mediaClassNames} data={data} />
+                        )
+                    }}
+                </Media>
+            </MediaContextProvider>
         </>)
 }
 
