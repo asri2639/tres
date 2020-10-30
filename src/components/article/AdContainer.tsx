0b6090da0478ea2fa0c956afad9f1d2ee2148c7a
Prashanth <prasanth@primaseller.com>
2020-10-31 00:29:38 +0530

    ads to articles

--- src/components/article/AdContainer.tsx
+++ src/components/article/AdContainer.tsx
@@ -6,8 +6,11 @@ const AdContainer = ({ data }) => {
         <>
             {
                 data.map(val => {
-                    if (val.list_type === 'ad_unit') {
-                        const [width, height] = val.ad_url.split('/').slice(-1)[0].split('x').map(v => parseInt(v))
+                    if (val.layout_type.indexOf('ad_unit') >= 0 && val.ad_url.length > 0) {
+                        const [width, height] = val.ad_url.split('/').slice(-1)[0].split('x').map(v => {
+                            const num = /\d+/.exec(v);
+                            return num ? +num[0] : 400
+                        })
                         return (
                             <iframe className="mx-auto" key={val.list_id} width={width + 50} height={height + 50} src={val.ad_url} />)
                     } else {
@@ -17,7 +20,6 @@ const AdContainer = ({ data }) => {
                                 return <PopularList key={val.list_id} data={val} />
                             case "four_plus_four_list":
                                 return <GridList key={val.list_id} data={val} />
-
 
                         }
                     }
