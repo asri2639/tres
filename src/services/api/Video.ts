3664a087039e72026a5489f1ab425d0a10a2b7af
Prashanth <prasanth@primaseller.com>
2020-11-12 12:49:31 +0530

    sending with no-cors mode

--- src/services/api/Video.ts
+++ src/services/api/Video.ts
@@ -19,8 +19,7 @@ export default function Video(inst) {
         }&auth_token=xBUKcKnXfngfrqGoF93y`,
         {
           method: 'GET', // *GET, POST, PUT, DELETE, etc.
-          // mode: 'no-cors', // no-cors, *cors, same-origin
-          mode: 'cors',
+          mode: 'no-cors', // no-cors, *cors, same-origin
           cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
           headers: {
             'Content-Type': 'application/json',
