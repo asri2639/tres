4152b066a0b86382c8ce5f2f699ce51ff3b60935
Prashanth <prasanth@primaseller.com>
2020-11-30 22:17:48 +0530

    optional config fix

--- src/interfaces/API.ts
+++ src/interfaces/API.ts
@@ -1,9 +1,10 @@
 import { AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios';
 
 export interface APIRequest extends AxiosRequestConfig {
+  params: any;
   query: { key: string; value: string };
   payload: any;
-  config: any;
+  config?: any;
 }
 
 export interface APIResponse extends AxiosResponse {}
