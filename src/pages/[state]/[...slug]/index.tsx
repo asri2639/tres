89268a48754eef4ab5aecdb03f8e34739b5533e0
Prashanth <prasanth@primaseller.com>
2020-12-09 20:05:40 +0530

    video page fix

--- src/pages/[state]/[...slug]/index.tsx
+++ src/pages/[state]/[...slug]/index.tsx
@@ -291,13 +291,8 @@ slug.getInitialProps = async ({ query, req, res, ...args }) => {
         }
         let suffix = null;
         if (applicationConfig.value) {
-          let convertedState = configStateCodeConverter(
-            location.pathname.split('/')[2]
-          );
-          convertedState =
-            location.pathname.split('/')[1] === 'urdu'
-              ? 'urdu'
-              : convertedState;
+          let convertedState = configStateCodeConverter(query.state);
+          convertedState = query.language === 'urdu' ? 'urdu' : convertedState;
 
           suffix =
             applicationConfig.value['params_hash2'].config_params.ssr_details[
