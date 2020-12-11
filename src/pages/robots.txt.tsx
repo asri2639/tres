8aff7b2cab102fb1ff229a57e54004bffc71f619
Prashanth <prasanth@primaseller.com>
2020-12-11 21:21:33 +0530

    robots txt fix

---
+++ src/pages/robots.txt.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import getConfig from 'next/config';
+
+const getRobots = (config) =>
+  config.APP_ENV === 'staging'
+    ? `User-agent: *
+Disallow: /
+`
+    : `User-agent: *
+Allow: /*
+
+Disallow: /api/*
+Disallow:/dai
+Disallow:/ads/
+Disallow:/dfpads
+Disallow:/etvbharat_staging
+Disallow:/react_etvbharat_staging
+Disallow:/etvbharat_staging
+Disallow:/gallery-ads
+Disallow:/home-page-ad-units
+Disallow:/in-article-ads
+Disallow:/banner-near-logo
+Disallow:/tamara-hospital
+Disallow:/1754*
+
+# Sitemap Files
+Sitemap: https://www.etvbharat.com/sitemap.xml
+Sitemap: https://www.etvbharat.com/latest_news_sitemap.xml
+`;
+
+class Sitemap extends React.Component {
+  public static async getInitialProps({ res }) {
+    const { publicRuntimeConfig } = getConfig();
+
+    res.setHeader('Content-Type', 'text/plain');
+    res.write(getRobots(publicRuntimeConfig));
+    res.end();
+  }
+}
+
+export default Sitemap;
