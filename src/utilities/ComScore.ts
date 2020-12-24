f2b7fe752f66f8c0f4317a5552f88962756b8f53
Prashanth <prasanth@primaseller.com>
2020-12-24 09:20:27 +0530

    comscore fix

--- src/utilities/ComScore.ts
+++ src/utilities/ComScore.ts
@@ -1,30 +1,33 @@
 export default {
   pageView: () => {
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        if (typeof window !== 'undefined') {
-          window['comscoreDisplay'] = JSON.parse(this.response);
-        }
-
-        // console.log($rootScope.comscoreDisplay.comscore + " Comscore Response");
-      }
-    };
-    request.open(
-      'GET',
-      location.origin +
-        '/comscore-pageview-candidate.json?' +
-        new Date().getTime(),
-      true
-    );
-    request.send();
   },
   nextPageView: () => {
     setTimeout(() => {
+      var request = new XMLHttpRequest();
+      request.onreadystatechange = function () {
+        console.log(111)
+        if (this.readyState == 4 && this.status == 200) {
+          if (typeof window !== 'undefined') {
+            window['comscoreDisplay'] = JSON.parse(this.response);
+          }
+  
+          // console.log($rootScope.comscoreDisplay.comscore + " Comscore Response");
+        }
+      };
+      request.open(
+        'GET',
+        location.origin +
+          '/comscore-pageview-candidate.json?' +
+          new Date().getTime(),
+        true
+      );
+      request.send();
+
       if (typeof window !== 'undefined' && window['COMSCORE']) {
         window['COMSCORE'] &&
-          window['COMSCORE'].beacon({ c1: '2', c2: 'CLIENT_ID' });
+          window['COMSCORE'].beacon({ c1: '2', c2: '20416623' });
       }
+
     }, 200);
   },
 };
