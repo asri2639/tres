37f3d6a6992dcdde92e5652e3492b945f750a9ed
Prashanth <prasanth@primaseller.com>
2020-12-31 12:02:00 +0530

    using router aspath instead of dynamic url

--- src/pages/[state]/[...slug]/index.tsx
+++ src/pages/[state]/[...slug]/index.tsx
@@ -62,7 +62,7 @@ const slug: NextPage<Propss> = ({ data, pageType, appConfig }) => {
         datum.contentType = data.content_type;
         datum.contentId = data.content_id;
 
-        canonicalUrl = `https://react.etvbharat.com${new URL(data.dynamic_url).pathname}`;
+        canonicalUrl = `https://react.etvbharat.com${router.asPath}`;
 
         return (
           <>
@@ -131,7 +131,7 @@ const slug: NextPage<Propss> = ({ data, pageType, appConfig }) => {
             'https://players-saranyu.s3.amazonaws.com/etvbharat_staging/saranyu_player/plugin/external-js/scroll-playpause1.js'
           );
         }
-        canonicalUrl = `https://react.etvbharat.com${new URL(data.dynamic_url).pathname}`;
+        canonicalUrl = `https://react.etvbharat.com${router.asPath}`;
 
         return (
           <>
@@ -188,11 +188,10 @@ const slug: NextPage<Propss> = ({ data, pageType, appConfig }) => {
         );
       case 'gallery':
         const main = data.gallery[0];
-
         const keywords = data.gallery.map((v) => {
           return v.description;
         });
-        canonicalUrl = `https://react.etvbharat.com${new URL(data.dynamic_url).pathname}`;
+        canonicalUrl = `https://react.etvbharat.com${router.asPath}`;
 
         return (
           <>
