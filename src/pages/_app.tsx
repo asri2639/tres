4ad6a34f318b12c346f9a61e2eaa8275565a5294
Prashanth-Primaseller <prasanth@primaseller.com>
2021-03-21 13:04:19 +0530

    removing all images to test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -94,24 +94,7 @@ function App({ Component, pageProps, data, accessToken, appConfig }) {
                     pageProps.data && pageProps.data.thumbnails
                       ? `<div class="title">${
                           pageProps.data.title
-                        }</div><div class="thumbnail"><img
-                src=${
-                  pageProps.data.media_type === 'breaking_news'
-                    ? 'https://react.etvbharat.com/assets/images/breakingplate.jpg'
-                    : pageProps.data.thumbnails &&
-                      pageProps.data.thumbnails.web_3_2
-                    ? pageProps.data.thumbnails.web_3_2.url
-                    : 'https://react.etvbharat.com/assets/images/breakingplate.jpg'
-                }
-                alt=${
-                  pageProps.data.media_type === 'breaking_news'
-                    ? 'Breaking News'
-                    : pageProps.data.thumbnails &&
-                      pageProps.data.thumbnails.web_3_2
-                    ? pageProps.data.thumbnails.web_3_2.alt_tags
-                    : 'Breaking News'
-                }
-              /></div>${pageProps.data.html_tag.replace(
+                        }</div></div>${pageProps.data.html_tag.replace(
                 /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi,
                 ''
               )}`
