8942142d05d272ec3b2789db5bc050fed69e4120
Prashanth-Primaseller <prasanth@primaseller.com>
2021-05-02 22:44:27 +0530

    infinite scroll custom hook added

---
+++ src/hooks/useInfiniteScroll.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+
+const useInfiniteScroll = (callback) => {
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  useEffect(() => {
+    if (!isFetching) return;
+    callback(() => {
+      console.log('called back');
+    });
+  }, [isFetching]);
+
+  function handleScroll() {
+    let offsetHeight = document.body.offsetHeight;
+    if (window.innerWidth < 769) {
+      offsetHeight -= 350;
+    }
+    if (window.innerHeight + window.pageYOffset < offsetHeight || isFetching)
+      return;
+    setIsFetching(true);
+  }
+
+  return [isFetching, setIsFetching];
+};
+
+export default useInfiniteScroll;
