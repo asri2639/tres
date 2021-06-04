72322851b2c1b68089a3304cce28f51d17682bd5
Prashanth-Primaseller <prasanth@primaseller.com>
2021-06-04 12:27:51 +0530

    env staging added to test

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -114,6 +114,8 @@ const Layout = ({ children, accessToken, appConfig, pageType }) => {
             region: country,
             response: 'r2',
             item_languages: language,
+
+            env: 'staging'
           },
         });
         const requiredData = result.data.data.params_hash2.footer;
@@ -179,6 +181,7 @@ const Layout = ({ children, accessToken, appConfig, pageType }) => {
           item_languages: language,
           portal_state: stateCodeConverter(state),
           only_items: 'catalog_list',
+          env: 'staging'
         },
         isSSR: !isDesktop,
       });
