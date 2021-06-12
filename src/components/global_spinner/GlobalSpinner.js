f51aad53d722038f24dcc266f7745e0fbd436a72
Prashanth-Primaseller <prasanth@primaseller.com>
2021-06-12 14:45:58 +0530

    loading spinner added

--- src/components/global_spinner/GlobalSpinner.js
+++ src/components/global_spinner/GlobalSpinner.js
@@ -4,7 +4,17 @@ import spinner from './globalSpinner.module.scss';
 const GlobalSpinner = (props) => {
   return (
     <div className={spinner['global-spinner-overlay']}>
-      <p>Loading...</p>
+      <div className="spinner sk-cube-grid">
+        <div className="sk-cube sk-cube1"></div>
+        <div className="sk-cube sk-cube2"></div>
+        <div className="sk-cube sk-cube3"></div>
+        <div className="sk-cube sk-cube4"></div>
+        <div className="sk-cube sk-cube5"></div>
+        <div className="sk-cube sk-cube6"></div>
+        <div className="sk-cube sk-cube7"></div>
+        <div className="sk-cube sk-cube8"></div>
+        <div className="sk-cube sk-cube9"></div>
+      </div>
     </div>
   );
 };
