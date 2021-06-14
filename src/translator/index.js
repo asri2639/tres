ee15ea8d4ef85b460ffa4d284ab9e310618a2450
Aditya Thadikonda <aditya.thadikonda@inductivequotient.com>
2021-06-14 20:07:27 +0530

    lazy loading

--- src/translator/index.js
+++ src/translator/index.js
@@ -1,67 +1,76 @@
- import english from './locales/en/common.json';
- import assamese from './locales/as/common.json';
- import bengali from './locales/bn/common.json';
- import gujarati from './locales/gu/common.json';
- import hindi from './locales/hi/common.json';
- import kannada from './locales/kn/common.json';
- import malayalam from './locales/ml/common.json';
- import marathi from './locales/mr/common.json';
- import oriya from './locales/or/common.json';
- import punjabi from './locales/pan/common.json';
- import tamil from './locales/ta/common.json';
- import telugu from './locales/te/common.json';
- import urdu from './locales/ur/common.json';
-
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
  export default function getTranslatedValue(key,language){
      let translatedValue = '';
 
      switch(language) {
          case 'en':
+            const english = require('./locales/en/common.json');
              translatedValue = english[`${key}`];
          break;
          case 'asm':
+            const assamese = require('./locales/as/common.json');
             translatedValue = assamese[`${key}`];
         break;
         case 'bn':
+            const bengali = require('./locales/bn/common.json');
             translatedValue = bengali[`${key}`];
         break;
 
         case 'gu':
+            const gujarati = require('./locales/gu/common.json');
             translatedValue = gujarati[`${key}`];
         break;
         case 'hi':
+            const hindi = require('./locales/hi/common.json');
             translatedValue = hindi[`${key}`];
         break;
         case 'kn':
+            const kannada  = require('./locales/kn/common.json');
             translatedValue = kannada[`${key}`];
         break;
 
         case 'ml':
+            const malayalam = require('./locales/ml/common.json');
             translatedValue = malayalam[`${key}`];
         break;
 
         case 'mr':
+            const marathi = require('./locales/mr/common.json');
             translatedValue = marathi[`${key}`];
         break;
 
         case 'or':
+            const oriya  = require('./locales/or/common.json');
             translatedValue = oriya[`${key}`];
         break;
 
         case 'pan':
+            const punjabi = require('./locales/pan/common.json');
             translatedValue = punjabi[`${key}`];
         break;
 
 
         case 'ta':
+            const tamil = require('./locales/ta/common.json');
             translatedValue = tamil[`${key}`];
         break;
 
         case 'te':
+            const telugu = require('./locales/te/common.json');
             translatedValue = telugu[`${key}`];
         break;
 
          case 'ur':
+            const urdu = require('./locales/ur/common.json');
             translatedValue = urdu[`${key}`];
         break;
 
