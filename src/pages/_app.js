1256f1c0b05cd82b23925b6a25c8624074d79531
Prashanth-Primaseller <prasanth@primaseller.com>
2021-07-14 15:25:45 +0530

    ads fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -90,7 +90,15 @@ function App({ Component, pageProps, data, accessToken, appConfig }) {
             type="text/javascript"
             src="//cdn.ergadx.com/js/889/ads.js"
           ></script>
-        ) : null}
+        ) : (
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(v,d,o,ai){ai=d.createElement('script');
+        ai.defer=true;ai.async=true;ai.src=v.location.protocol+o;d.head.appendChild(ai);})(window, document, '//a.vdo.ai/core/v-etvbharat/vdo.ai.js');
+`,
+            }}
+          ></script>
+        )}
 
         <link
           rel="preload"
@@ -214,12 +222,3 @@ App.getInitialProps = async ({ Component, ctx }) => {
 };
 
 export default App;
-
-
-{/* <script
-dangerouslySetInnerHTML={{
-  __html: `(function(v,d,o,ai){ai=d.createElement('script');
-ai.defer=true;ai.async=true;ai.src=v.location.protocol+o;d.head.appendChild(ai);})(window, document, '//a.vdo.ai/core/v-etvbharat/vdo.ai.js');
-`,
-}}
-></script> */}
