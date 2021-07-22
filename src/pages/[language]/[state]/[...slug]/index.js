b54fb2015b4abb6c8fa593c3e21e1fc5a1996242
Aditya Thadikonda <aditya.thadikonda@inductivequotient.com>
2021-07-22 06:47:57 +0530

    'pagetitleforlistingpages'

--- src/pages/[language]/[state]/[...slug]/index.js
+++ src/pages/[language]/[state]/[...slug]/index.js
@@ -301,7 +301,7 @@ const { appLanguage } = useTranslator();
 
         return (<>
           <Head>
-              <title>{(data.meta_tag_title !== '' && data.meta_tag_title.includes('Add canonical tag')) ? data.meta_tag_title : 'ETV Bharat'}</title>
+              <title>{(data.meta_tag_title !== '' && !data.meta_tag_title.includes('Add canonical tag')) ? data.meta_tag_title : 'ETV Bharat'}</title>
           </Head>;
           <NextSeo
             title={data.meta_tag_title }
