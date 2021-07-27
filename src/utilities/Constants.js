6f627ea8fdafccca6f85c9ef46a0e1f75105679f
Prashanth-KR <kr.prasanth@delhivery.com>
2021-07-27 20:24:09 +0530

    empty domain url

--- src/utilities/Constants.js
+++ src/utilities/Constants.js
@@ -14,7 +14,7 @@ const baseURL =
 
 export const domainUrl =
   env === 'production' && !prd_test
-    ? 'https://www.etvbharat.com'
+    ? ''
     : env === 'development' || prd_test
     ? 'http://localhost:3000'
     : 'https://staging.etvbharat.com';
