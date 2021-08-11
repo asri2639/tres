4a8b69b7a117528f035cb4993b0bffc82659fa64
Prashanth-KR <kr.prasanth@delhivery.com>
2021-08-11 19:51:25 +0530

    robots txt updated

--- src/pages/api/robots.js
+++ src/pages/api/robots.js
@@ -25,7 +25,29 @@ Disallow:/*%
 # Sitemap Files
 Sitemap: https://www.etvbharat.com/googlenewssitemap.xml
 Sitemap: https://www.etvbharat.com/sitemap.xml
-Sitemap: https://www.etvbharat.com/category-sitemap.xml`;
+Sitemap: https://www.etvbharat.com/category-sitemap.xml
+Sitemap: https://www.etvbharat.com/hindi/bihar/latest_news_sitemap.xml
+Sitemap: https://www.etvbharat.com/hindi/chhattisgarh/latest_news_sitemap.xml
+Sitemap: https://www.etvbharat.com/gujarati/gujarat/latest_news_sitemap.xml
+Sitemap: https://www.etvbharat.com/hindi/himachal-pradesh/latest_news_sitemap.xml
+Sitemap: https://www.etvbharat.com/hindi/jharkhand/latest_news_sitemap.xml
+Sitemap: https://www.etvbharat.com/oriya/odisha/latest_news_sitemap.xml
+Sitemap: https://www.etvbharat.com/punjabi/punjab/latest_news_sitemap.xml
+Sitemap: https://www.etvbharat.com/hindi/rajasthan/latest_news_sitemap.xml
+Sitemap: https://www.etvbharat.com/hindi/uttarakhand/latest_news_sitemap.xml
+Sitemap: https://www.etvbharat.com/assamese/assam/latest_news_sitemap.xml
+Sitemap: https://www.etvbharat.com/hindi/haryana/latest_news_sitemap.xml
+Sitemap: https://www.etvbharat.com/tamil/tamil-nadu/latest_news_sitemap.xml
+Sitemap: https://www.etvbharat.com/malayalam/kerala/latest_news_sitemap.xml
+Sitemap: https://www.etvbharat.com/urdu/jammu-and-kashmir/latest_news_sitemap.xml
+Sitemap: https://www.etvbharat.com/urdu/urdu-national/latest_news_sitemap.xml
+Sitemap: https://www.etvbharat.com/hindi/delhi/latest_news_sitemap.xml
+Sitemap: https://www.etvbharat.com/marathi/maharashtra/latest_news_sitemap.xml
+Sitemap: https://www.etvbharat.com/telugu/telangana/latest_news_sitemap.xml
+Sitemap: https://www.etvbharat.com/bengali/west-bengal/latest_news_sitemap.xml
+Sitemap: https://www.etvbharat.com/hindi/uttar-pradesh/latest_news_sitemap.xml
+Sitemap: https://www.etvbharat.com/kannada/karnataka/latest_news_sitemap.xml
+Sitemap: https://www.etvbharat.com/hindi/madhya-pradesh/latest_news_sitemap.xml`;
 
 /* class Sitemap extends React.Component {
   static async getInitialProps({ res }) {
