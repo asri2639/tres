2e434bf095ebf8f76280c603112223a56d3e02dc
Prashanth-KR <kr.prasanth@delhivery.com>
2021-08-12 21:48:55 +0530

    css purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
       colors: {
         mbg: 'var(--color-mb-background)',
         hbg: '#031820',
-        mhbg: '#052e43'
+        mhbg: '#052e43',
       },
       fontSize: {
         tiny: '0.9375rem',
@@ -22,13 +22,38 @@ module.exports = {
         '-10': '-10',
       },
       fontFamily: {
-        english: ['LikhithEnglish']
+        english: ['LikhithEnglish'],
       },
       boxShadow: {
         t: '0 -2px 13px rgba(0,0,0,0.2)',
-      }
+      },
     },
   },
   variants: {},
-  plugins: [],
+  plugins: [
+    'postcss-flexbugs-fixes',
+    [
+      'postcss-preset-env',
+      {
+        autoprefixer: {
+          flexbox: 'no-2009',
+        },
+        stage: 3,
+        features: {
+          'custom-properties': false,
+        },
+      },
+    ],
+    [
+      '@fullhuman/postcss-purgecss',
+      {
+        content: [
+          './pages/**/*.{js,jsx,ts,tsx}',
+          './components/**/*.{js,jsx,ts,tsx}',
+        ],
+        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+        safelist: ['html', 'body'],
+      },
+    ],
+  ],
 };
