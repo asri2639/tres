32a958609ee5649269b8b85a83de061dfce5b14c
Prashanth-KR <kr.prasanth@delhivery.com>
2021-08-12 22:52:12 +0530

    preload fonts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -75,6 +75,43 @@ function App({ Component, pageProps, data, accessToken, appConfig }) {
         <meta name="bingbots" content="all" />
         <meta name="robots" content="all" />
         <meta name="theme-color" content="#07254c" />
+
+        {!isAMP ? (
+          <>
+            {langName !== 'english' ? (
+              <>
+                <link
+                  rel="preload"
+                  href={`https://etvbharatimages.akamaized.net/etvbharat/static/assets/fonts/${langCap}/${langName}.woff2`}
+                  as="font"
+                  type="font/woff2"
+                  crossOrigin={'anonymous'}
+                />
+                <link
+                  rel="preload"
+                  href={`https://etvbharatimages.akamaized.net/etvbharat/static/assets/fonts/${langCap}/${langName}.woff`}
+                  as="font"
+                  type="font/woff2"
+                  crossOrigin={'anonymous'}
+                />
+              </>
+            ) : null}
+            <link
+              rel="preload"
+              href="https://etvbharatimages.akamaized.net/etvbharat/static/assets/fonts/English/english.woff2"
+              as="font"
+              type="font/woff2"
+              crossOrigin={'anonymous'}
+            />
+            <link
+              rel="preload"
+              href="https://etvbharatimages.akamaized.net/etvbharat/static/assets/fonts/English/english.woff"
+              as="font"
+              type="font/woff2"
+              crossOrigin={'anonymous'}
+            />
+          </>
+        ) : null}
       </Head>
 
       {accessToken.web.length && accessToken.mobile.length ? (
