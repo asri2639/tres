0b2d7db33dcbb28c7f853349f8c3aba4a26af1e5
kr-prashanth <kr.prasanth@delhivery.com>
2021-09-16 19:09:48 +0530

    access token updated

--- src/static/accessToken.js
+++ src/static/accessToken.js
@@ -7,8 +7,13 @@ export default {
     web: 'TjeNsXehJqhh2DGJzBY9',
     mobile: 'woB1UukKSzZ5aduEUxwt',
   },
+
   staging: {
+    web: 'TjeNsXehJqhh2DGJzBY9',
+    mobile: 'woB1UukKSzZ5aduEUxwt',
+  },
+  /*  staging: {
     web: 'Fw3196P2FES3HBfytEtn',
     mobile: 's2byd1VP6PN8hYDsb8zj',
-  },
+  }, */
 };
