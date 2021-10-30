6c5ccd3402ab8465120765ec285900c49d1d9816
k-r-prashanth <prasanthk@viustats.com>
2021-10-30 12:07:40 +0530

    cache control added

--- src/pages/api/appads.js
+++ src/pages/api/appads.js
@@ -9,6 +9,7 @@ export default (req, res) => {
   const { publicRuntimeConfig } = getConfig();
 
   res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Cache-Control', 's-maxage=86400, stale-while-revalidate');
   res.write(getAds(publicRuntimeConfig));
   res.end();
 };
--- src/pages/api/robots.js
+++ src/pages/api/robots.js
@@ -26,6 +26,7 @@ export default (req, res) => {
   const { publicRuntimeConfig } = getConfig();
 
   res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Cache-Control', 's-maxage=86400, stale-while-revalidate');
   res.write(getRobots(publicRuntimeConfig));
   res.end();
 };
