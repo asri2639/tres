91a2a610896d16656a648525a0e96f24e0664def
aditya thadikonda <adityathadikonda@adityas-MacBook-Pro.local>
2021-12-02 21:48:17 +0530

    staging

--- src/services/api/FileFetcher.js
+++ src/services/api/FileFetcher.js
@@ -7,15 +7,11 @@ export default {
     return import('../../static/footer').then((r) => r.default);
   },
   getAppConfig({ params, query, ...config }) {
-    if(env === "staging"){
-      return import('../../static/stagingAppconfig')
-      .then((r) => r.default)
-      .catch((e) => {});
-    }else{
+    
       return import('../../static/appConfig')
       .then((r) => r.default)
       .catch((e) => {});
-    }
+    
     
   },
   
