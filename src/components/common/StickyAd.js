468901c989066ef9b336756014093a03530fe6c0
kr-prashanth <kr.prasanth@delhivery.com>
2021-12-29 15:58:13 +0530

    set timeout for sticky ad load

--- src/components/common/StickyAd.js
+++ src/components/common/StickyAd.js
@@ -19,38 +19,40 @@ const StickyAd = ({ className, refresh }) => {
 
   useEffect(() => {
     if (!isTransitioning && typeof window !== undefined && isDesktop != null) {
-      if (adData && adData.ad_unit) {
-        window.ads = window.ads || new Set();
-        const ads = window.ads;
+      setTimeout(() => {
+        if (adData && adData.ad_unit) {
+          window.ads = window.ads || new Set();
+          const ads = window.ads;
 
-        if (!ads.has(adData.gpt_id)) {
-          if (adEl.current) {
-            if (window.googletag && googletag.apiReady) {
-              window[adData.gpt_id] = googletag
-                .defineSlot(adData.ad_unit, [[300, 50]], adData.gpt_id)
-                .addService(googletag.pubads());
+          if (!ads.has(adData.gpt_id)) {
+            if (adEl.current) {
+              if (window.googletag && googletag.apiReady) {
+                window[adData.gpt_id] = googletag
+                  .defineSlot(adData.ad_unit, [[300, 50]], adData.gpt_id)
+                  .addService(googletag.pubads());
 
-              googletag.enableServices();
+                googletag.enableServices();
 
-              googletag.cmd.push(function () {
-                googletag.display(adData.gpt_id);
-              });
+                googletag.cmd.push(function () {
+                  googletag.display(adData.gpt_id);
+                });
 
-              window.ads.add(adData.gpt_id);
+                window.ads.add(adData.gpt_id);
+              }
+            }
+          } else {
+            if (adEl.current) {
+              setTimeout(() => {
+                if (adEl.current && !adEl.current.querySelector('iframe')) {
+                  googletag.cmd.push(function () {
+                    googletag.pubads().refresh([window[adData.gpt_id]]);
+                  });
+                }
+              }, 300);
             }
           }
-        } else {
-          if (adEl.current) {
-            setTimeout(() => {
-              if (adEl.current && !adEl.current.querySelector('iframe')) {
-                googletag.cmd.push(function () {
-                  googletag.pubads().refresh([window[adData.gpt_id]]);
-                });
-              }
-            }, 300);
-          }
         }
-      }
+      }, 300);
     }
   }, [adData, isDesktop, adEl]);
 
