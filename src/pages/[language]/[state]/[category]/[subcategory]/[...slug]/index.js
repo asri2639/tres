531780a7cca889288cc0fe5430d3211164b9df0f
Aditya S <adityaa@gmail.com>
2022-04-25 20:16:14 +0530

    ld json fix

--- src/pages/[language]/[state]/[category]/[subcategory]/[...slug]/index.js
+++ src/pages/[language]/[state]/[category]/[subcategory]/[...slug]/index.js
@@ -349,6 +349,7 @@ const slug = ({ data, initCount, pageType, id, payload, dropDownData }) => {
  "mainEntityOfPage": {
  "@type": "WebPage",
  "@id": "https://www.etvbharat.com/${headerObj.url}"
+ "keywords":"${headerObj.keywords}"
  },
  "headline": "${headerObj.headline}",
  "description": "${headerObj.description.replace(/\"/gi, '\\"')}",
