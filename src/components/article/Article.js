595c002466abf7db885f4cb5bbf9711295f2eaef
Aditya S <adityaa@gmail.com>
2022-05-18 10:13:11 +0530

    taboola all changes

--- src/components/article/Article.js
+++ src/components/article/Article.js
@@ -408,11 +408,11 @@ export default function Article({
                 </div>
               ) : null}
               {
-                (!infiniteTaboola && htmlShow) ? <TaboolaAd index={index} url={'https://preprod.etvbharat.com/${data.web_url}'} />: null
+                (nextArticle !== null && htmlShow) ? <TaboolaAd index={index} url={'https://preprod.etvbharat.com/${data.web_url}'} />: null
               }
-             
+             {console.log('infinite',nextArticle,htmlShow)}
             {
-              (nextArticle === null && htmlShow) ? (<InfiniteTaboolaAd index={index} url={'https://preprod.etvbharat.com/${data.web_url}'} />): null
+              (nextArticle === null && index !== 0 && htmlShow) ? (<InfiniteTaboolaAd index={index} url={'https://preprod.etvbharat.com/${data.web_url}'} />): null
             }
               
             </div>
