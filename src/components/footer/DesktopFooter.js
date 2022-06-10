5caee10e024edd8ed4136b86e4d08398fa72d83d
Aditya S <adityaa@gmail.com>
2022-06-10 21:03:51 +0530

    adgebra fix

--- src/components/footer/DesktopFooter.js
+++ src/components/footer/DesktopFooter.js
@@ -193,10 +193,26 @@ const DesktopFooter = ({ data, t }) => {
             console.log('cgj',state)
           }
           {
-            state === 'haryana' ? (<div dangerouslySetInnerHTML={{
+            state === 'haryana' ? (<>
+             <script
+            dangerouslySetInnerHTML={{
+              __html: `var adgf_p1 = "5795";
+              var adgf_p2 = "0";
+              var adgf_p3 = "0";
+              var adgf_p4 = "";
+              var adgf_p5 = "";
+              var adgFtSc = document.createElement("script");
+              adgFtSc.id = "adg_pixel_footer_script";
+              document.body.appendChild(adgFtSc);
+              adgFtSc.async = true;
+              var adgScParam = "p1=" + adgf_p1 + "&amp;p2=" + adgf_p2 + "&amp;p3=" + adgf_p3 + "&amp;p4=" + adgf_p4 + "&amp;p5=" + adgf_p5;
+              adgFtSc.src = "//adgebra.co.in/afpf/afpf.js?" + adgScParam;`
+            }}>
+            </script>
+            <div dangerouslySetInnerHTML={{
               __html:`<noscript>	<iframe id='adg_footer_pixel_script' src="//adgebra.co.in/afpf/afpf?p1=5795&amp;p2=0&amp;p3=1&amp;p4=&amp;p5=" width="0" height="0" frameBorder="0" scrolling="no" marginheight="0" marginwidth="0">
             </iframe>
-            </noscript>`}} />):null
+            </noscript>`}} /></>):null
           }
           
           <div className="text-center text-sm text-gray-500">
